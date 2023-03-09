9948a10925f8a78d89125b5368be60a936eec98e
SY <97646668+drsuneamer@users.noreply.github.com>
2023-03-09 15:28:18 +0900

    B11660_구간합구하기5_신선영

---
+++ src/Day1/B11660_구간합구하기2/B11660_구간합구하기5_신선영.java
@@ -0,0 +1,47 @@
+package day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11660_구간합구하기5_신선영 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N + 1][N + 1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if (i == 0 && j == 0) {
+                    arr[i + 1][j + 1] = n;
+                } else {
+                    if (i == 0) {
+                        arr[i + 1][j + 1] = arr[i + 1][j] + n;
+                    } else if (j == 0) {   // 첫 줄의 경우
+                        arr[i + 1][j + 1] = arr[i][j + 1] + n;
+                    } else {
+                        arr[i + 1][j + 1] = arr[i][j + 1] + arr[i + 1][j] - arr[i][j] + n;
+                    }
+                }
+            }
+
+        }
+
+        for (int i = 0; i < M; i++) {   // 테스트케이스
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            System.out.println(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1]);
+
+        }
+    }
+}
