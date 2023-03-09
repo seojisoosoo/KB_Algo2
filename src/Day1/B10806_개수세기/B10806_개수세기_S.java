0f1a37a02d4238420e21785552c8d17b5ca3b8e9
jaenGit <dksehgis@androidjava.com>
2023-03-09 14:36:37 +0900

    add ArrayTest

--- src/Day1/B10806_개수세기/B10806_개수세기_S.java
+++ src/Day1/B10806_개수세기/B10806_개수세기_S.java
@@ -2,7 +2,7 @@ package Day1.B10806_개수세기;
 
 import java.util.StringTokenizer;
 
-public class B10806_개수세기_안도현 {
+public class B10806_개수세기_S {
 
 	
 	public static void main(String[] args) {
--- src/Day1/B11660_구간합구하기2/B11660_구간합구하기2_안도현.java
+++
@@ -1,5 +0,0 @@
-package Day1.B11660_구간합구하기2;
-
-public class B11660_구간합구하기2_안도현 {
-
-}
