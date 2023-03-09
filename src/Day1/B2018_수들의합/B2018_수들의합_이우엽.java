e6d96ccbdf540a259892dd05848c4816447cfc9b
leewooyup <zjadlspun1114@naver.com>
2023-03-09 16:30:17 +0900

    B2018_수들의합_이우엽

--- src/Day1/B2018_수들의합/B2018_수들의합_이우엽.java
+++ src/Day1/B2018_수들의합/B2018_수들의합_이우엽.java
@@ -16,25 +16,17 @@ public class B2018_수들의합_이우엽 {
 			int sum = i;
 			int end = i+1;
 			for(int j = i+1; j <= end; j++) {
-				System.out.println("i: " + i);
-				System.out.println("j: " + j);
-				System.out.println("end: " + end);
 				// 조건을 만족하면 break
 				if(sum == N) {
-					System.out.println("15!!: " + sum);
 					result++;
-					System.out.println("==============");
 					break;
 				}
 				if(sum > N) {
 					break;
 				}
-				System.out.println("before sum: " + sum);
 				sum += j;
-				System.out.println("after sum: " + sum);
 				end++;
 			}
-			System.out.println("--------------");
 		}
 		System.out.println(result);
 		
