ff0991eab8967cae7ee69f6410ddcc213abbdbad
jaenGit <dksehgis@androidjava.com>
2023-03-09 14:34:15 +0900

    B11659_구간합구하기_S

---
+++ src/Day1/B11659_구간합구하기/B11659_구간합구하기_S.java
@@ -0,0 +1,34 @@
+package Day1.B11659_구간합구하기;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B11659_구간합구하기_S {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(reader.readLine() , " ");
+		int N = Integer.parseInt(str.nextToken()); 
+		int M = Integer.parseInt(str.nextToken());
+		
+		int [] arr = new int [N + 1];
+		str = new StringTokenizer(reader.readLine() , " ");
+		arr[0] =  0;
+		
+		for (int i = 0; i < N; i++) {	
+			arr[i + 1] = Integer.parseInt(str.nextToken()) + arr[i];
+		}
+		
+		for (int i = 0; i < M; i++) {
+			str = new StringTokenizer(reader.readLine() , " ");
+			int start = Integer.parseInt(str.nextToken());
+			int end = Integer.parseInt(str.nextToken());
+			System.out.println(arr[end] - arr[start - 1]);
+		}
+	}
+}
+
