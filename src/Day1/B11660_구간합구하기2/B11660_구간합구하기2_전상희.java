91f376aab5786e6eafefe63cfd4276addf97eaaa
ybwi0912 <ybwi0912@naver.com>
2023-03-09 16:57:04 +0900

    B2018_수들의합_전상희

--- src/Day1/B11660_구간합구하기2/B11660_구간합구하기2_전상희.java
+++ src/Day1/B11660_구간합구하기2/B11660_구간합구하기2_전상희.java
@@ -3,6 +3,7 @@ package Day1.B11660_구간합구하기2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B11660_구간합구하기2_전상희 {
@@ -21,18 +22,21 @@ public class B11660_구간합구하기2_전상희 {
 			token = new StringTokenizer(bf.readLine(), " ");
 			for(int j=0; j<N; j++) {
 				arr[i][j] = Integer.parseInt(token.nextToken());			
-			}
-			
+			}			
 		}
+		
 		for(int i=0; i<N; i++) {
 			sArr[i+1][1] = sArr[i][1] + arr[i][0];
 			sArr[1][i+1] = sArr[1][i] + arr[0][i];
 		}
+		
 		for(int i=2; i<N+1; i++) {
 			for(int j=2; j<N+1; j++) {
 				sArr[i][j] = sArr[i-1][j] + sArr[i][j-1] - sArr[i-1][j-1] + arr[i-1][j-1];
 			}			
 		}
+		
+		System.out.println(Arrays.deepToString(sArr));
 		
 		int[] ans = new int[M];
 		
@@ -42,14 +46,11 @@ public class B11660_구간합구하기2_전상희 {
 			int y1 = Integer.parseInt(token.nextToken());
 			int x2 = Integer.parseInt(token.nextToken());
 			int y2 = Integer.parseInt(token.nextToken());
-			ans[i] = sArr[x2][y2] -sArr[x1-1][y2] - sArr[x2][y1-1] + sArr[x1-1][y1-1];
-			
+			ans[i] = sArr[x2][y2] -sArr[x1-1][y2] - sArr[x2][y1-1] + sArr[x1-1][y1-1];	
 		}
 		
 		for(int a : ans) {
 			System.out.println(a);
-		}
-		
+		}	
 	}
-
 }
