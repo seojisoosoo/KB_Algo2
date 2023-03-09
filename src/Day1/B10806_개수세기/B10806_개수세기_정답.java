fa81fd3b81ddab587a527d36a2bec67c46de84ac
orozg <whdkdud3710@gmail.com>
2023-03-09 11:23:14 +0900

    B10806_개수세기_조아영

--- src/Day1/B10806_개수세기/B10806_개수세기_정답.java
+++ src/Day1/B10806_개수세기/B10806_개수세기_정답.java
@@ -8,22 +8,30 @@ import java.util.StringTokenizer;
 
 public class B10806_개수세기_정답 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-	
-		// 입력 처리 //
+
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt( bf.readLine());
-		String s = bf.readLine();
-		int [] arr = new int [N];
-		StringTokenizer token = new StringTokenizer(s , " ");
-		for(int i=0; i<N; i++) {
+		
+		//bf.readLine() 문자열을 정수형으로 변환
+		int n = Integer.parseInt(bf.readLine());		
+		
+		//StringTokenizer(,구분자)
+		StringTokenizer token = new StringTokenizer(bf.readLine());
+		
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(token.nextToken());
 		}
+		
 		int v = Integer.parseInt(bf.readLine());
+		
+		int ans = 0;
+		for(int i = 0; i<n; i++) {
+			if(v == arr[i]) {
+				ans++;
+			}		
+		}		
+		System.out.println(ans);
 
-        // v 찾아서 카운트 //
-        int ans = 0;
-		for(int i=0; i<N; i++) 
-			if(arr[i] == v) ans++;
-        System.out.println(ans);
 	}
 }
---
+++ src/Day1/B10806_개수세기/B10806_개수세기_조아영.java
@@ -0,0 +1,10 @@
+package Day1.B10806_개수세기;
+
+public class B10806_개수세기_조아영 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
