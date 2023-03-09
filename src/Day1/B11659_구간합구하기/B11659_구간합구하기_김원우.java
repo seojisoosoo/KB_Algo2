644a11a7a208a28bae7c2f928129ba7fd1b82173
jaenGit <dksehgis@androidjava.com>
2023-03-09 13:31:43 +0900

    update

--- src/Day1/B11659_구간합구하기/B11659_구간합구하기_김원우.java
+++ src/Day1/B11659_구간합구하기/B11659_구간합구하기_김원우.java
@@ -1,4 +1,4 @@
-package Day1.B2018_수들의합;
+package Day1.B11659_구간합구하기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class B2018_수들의합_김원우 {
+public class B11659_구간합구하기_김원우 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
