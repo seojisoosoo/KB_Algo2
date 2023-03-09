cf5d7afe57d9e7d5f708d51af5493ba53e9de019
rninji <yellowminji@naver.com>
2023-03-09 15:06:45 +0900

    B11660_구간합구하기2_길민지

--- src/Day1/B11660_구간합구하기2/B11660_구간합구하기2_길민지.java
+++ src/Day1/B11660_구간합구하기2/B11660_구간합구하기2_길민지.java
@@ -18,19 +18,19 @@ public class B11660_구간합구하기2_길민지 {
 		
 		// 배열 입력
 		for (int i=1; i<N+1; i++) {
-			StringTokenizer st2 = new StringTokenizer(bf.readLine());
+			st = new StringTokenizer(bf.readLine());
 			for (int j=1; j<N+1; j++) {
-				arr[i][j] = arr[i-1][j] + arr[i][j-1] + Integer.parseInt(st2.nextToken()) - arr[i-1][j-1];
+				arr[i][j] = arr[i-1][j] + arr[i][j-1] + Integer.parseInt(st.nextToken()) - arr[i-1][j-1];
 			}
 		}
 		
 		// 네 개의 정수 입력
 		for (int i=0; i<M; i++) {
-			StringTokenizer st3 = new StringTokenizer(bf.readLine());
-			int x1 = Integer.parseInt(st3.nextToken());
-			int y1 = Integer.parseInt(st3.nextToken());
-			int x2 = Integer.parseInt(st3.nextToken());
-			int y2 = Integer.parseInt(st3.nextToken());
+			st = new StringTokenizer(bf.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
 			System.out.println(arr[x2][y2]-arr[x2][y1-1]-arr[x1-1][y2]+arr[x1-1][y1-1]);
 		}
 	}
