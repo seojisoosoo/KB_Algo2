482604a4cb711523598f53357506502744b0a720
SY <97646668+drsuneamer@users.noreply.github.com>
2023-03-09 13:50:09 +0900

    B11659_구간합구하기_신선영

---
+++ src/Day1/B11659_구간합구하기/B11659_구간합구하기_신선영.java
@@ -0,0 +1,41 @@
+package day1;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11659_구간합구하기_신선영 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        arr[0] = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int t = 0; t < M; t++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            if (s == 1) {
+                System.out.println(arr[e - 1]);
+            } else if (s == e){
+                System.out.println(arr[e - 1] - arr[e - 2]);
+            } else {
+                System.out.println(arr[e - 1] - arr[s - 2]);
+            }
+
+        }
+    }
+}
