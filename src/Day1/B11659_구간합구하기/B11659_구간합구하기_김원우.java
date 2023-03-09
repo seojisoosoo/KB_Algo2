8d16ed8a943626711450c5521383da1dc6fc30d8
Awarduuu <award_uuu@naver.com>
2023-03-09 15:47:28 +0900

    B11660_구간합구하기2_윤상우

--- src/Day1/B11659_구간합구하기/B11659_구간합구하기_김원우.java
+++ src/Day1/B11659_구간합구하기/B11659_구간합구하기_김원우.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class B11659_구간합구하기4_김원우 {
+public class B11659_구간합구하기_김원우 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
