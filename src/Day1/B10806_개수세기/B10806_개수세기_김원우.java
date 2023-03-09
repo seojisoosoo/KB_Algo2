52081c635d4d6c00de7d552e80ed95c8193ece0a
Awarduuu <award_uuu@naver.com>
2023-03-09 14:27:27 +0900

    B11659_구간합구하기_윤상우

--- src/Day1/B10806_개수세기/B10806_개수세기_김원우.java
+++ src/Day1/B10806_개수세기/B10806_개수세기_김원우.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baekjoon_10807 {
+public class B10806_개수세기_김원우 {
     public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
