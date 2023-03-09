b79541c9f2beec05973973407e925ed8fd285487
김준서 <adoo244475@gmail.com>
2023-03-09 14:34:58 +0900

    김준서 제출

---
+++ src/Day1/B2018_수들의합/B2018_수들의합_김준서.java
@@ -0,0 +1,22 @@
+package Day1.B2018_수들의합;
+
+import java.util.Scanner;
+
+public class B2018_수들의합_김준서 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            int sum=0;
+            for (int j = i; j <= N; j++) {
+                sum+=j;
+                if (sum > N) {
+                    break;
+                }
+                dp[sum]+=1;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
