f4b256e68fefc29306d462b26edefd45447c76e0
gyeoul <clsmay.1st@gmail.com>
2023-03-13 13:18:39 +0900

    B2750_삽입정렬_박창현

---
+++ src/Day3/B2750_삽입정렬/B2750_삽입정렬_박창현.java
@@ -0,0 +1,55 @@
+package Day3.B2750_삽입정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// B2750_버블정렬
+public class B2750_삽입정렬_박창현 {
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[min]>arr[j]) min = j;
+            }
+            swap(arr,i,min);
+        }
+    }
+
+    public static void insertionSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int a = arr[i];
+            for (int j = i-1; j >= 0; j--) {
+                if(arr[j]>a) swap(arr,j+1,j);
+                else break;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        insertionSort(arr);
+        //System.out.println(Arrays.toString(arr));
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
