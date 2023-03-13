d87178084df2e8951e7fe2fe8a2dc7a9a88be49d
orozg <whdkdud3710@gmail.com>
2023-03-13 11:06:47 +0900

    B2750_버블정렬_조아영

--- src/Day3/B2750_버블정렬/B2750_버블정렬_조아영.java
+++ src/Day3/B2750_버블정렬/B2750_버블정렬_조아영.java
@@ -16,9 +16,9 @@ public class B2750_버블정렬_조아영 {
 
 	public static void bubblesort(int[] arr) {
 
-		for (int i = arr.length - 1; i > 0; i--) { 
-			for (int j = 0; j < i; j++) { 
-				if (arr[j] > arr[j + 1]) {
+		for (int i = arr.length - 1; i > 0; i--) { 	//i=4
+			for (int j = 0; j < i; j++) { //i=4, j=0~3
+				if (arr[j] > arr[j + 1]) {	//arr[0] >arr[1]
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
@@ -27,7 +27,10 @@ public class B2750_버블정렬_조아영 {
 			}
 		}
 
-		System.out.println(Arrays.toString(arr));
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+//		System.out.println(Arrays.toString(arr));
 
 	}
 
