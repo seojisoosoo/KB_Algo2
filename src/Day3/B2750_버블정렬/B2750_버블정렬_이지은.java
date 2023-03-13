5f9bb75bef23387f4c458820af8456049f732141
lje71421@naver.com <lje71421@naver.com>
2023-03-13 10:59:43 +0900

    B2750_버블정렬_이지은

--- src/Day3/B2750_버블정렬/B2750_버블정렬_이지은.java
+++ src/Day3/B2750_버블정렬/B2750_버블정렬_이지은.java
@@ -21,15 +21,12 @@ public class B2750_버블정렬_이지은 {
 	
 	public static void bubbleSort(int[] arr) {
 		for(int i=0; i<arr.length; i++) {
-			while(true) {
-				for(int j=1; j<arr.length-i; j++) {
-					if(arr[j-1]>arr[j]) {
-						swap(arr, j-1, j);
-					}else {
-						continue;
-					}
+			for(int j=1; j<arr.length-i; j++) {
+				if(arr[j-1]>arr[j]) {
+					swap(arr, j-1, j);
+				}else {
+					continue;
 				}
-				break;
 			}
 		}
 		for(int a: arr) {
