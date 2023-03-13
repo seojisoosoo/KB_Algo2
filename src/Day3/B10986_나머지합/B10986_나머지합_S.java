206a3ed3ddf579de3a20d5ef00ff9df8218e03e1
jaenGit <dksehgis@androidjava.com>
2023-03-13 09:51:00 +0900

    B10986_나머지합_S

--- src/Day3/B10986_나머지합/B10986_나머지합_S.java
+++ src/Day3/B10986_나머지합/B10986_나머지합_S.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class B10986_나머지합 {
+public class B10986_나머지합_S {
 
 	public static void main(String[] args) throws IOException {
 		
