d851ef39a720ff86c956f38456a93f31e1ac9b91
gyeoul <clsmay.1st@gmail.com>
2023-03-14 14:13:31 +0900

    B9742_순열_박창현_코드클리닝

--- src/Day4/B9742_순열/B9742_순열_박창현.java
+++ src/Day4/B9742_순열/B9742_순열_박창현.java
@@ -6,28 +6,19 @@ import java.io.InputStreamReader;
 
 public class B9742_순열_박창현 {
     static char[] rawArr;
-    static int res;
     static int count;
     static int[] selArr;
     static boolean[] isIn;
-    static int N;
     static String[] s;
 
     static void permutation(int r, StringBuilder sb) {
-        if (r == N) {
-            if (count == res - 1) {
-                sb.append(s[0]).append(" ").append(s[1]).append(" = ");
-                //System.out.print(s[0] + " "+s[1]+" = " );
+        if (r == rawArr.length) {
+            if (count == Integer.parseInt(s[1])) {
                 for (int i = 0; i < r; i++) {
-                    //System.out.print(rawArr[selArr[i]]);
                     sb.append(rawArr[selArr[i]]);
                 }
-                sb.append("\n");
             }
-            //System.out.println();
             count++;
-//            count--;
-//            count++;
             return;
         }
         for (int i = 0; i < rawArr.length; i++) {
@@ -38,38 +29,23 @@ public class B9742_순열_박창현 {
             isIn[i] = false;
         }
     }
-//    static void action(String[] s,StringBuilder sb){
-//        rawArr = s[0].toCharArray();
-//        res = Integer.parseInt(s[1]);
-//        N = rawArr.length;
-//        selArr = new int[rawArr.length];
-//        isIn = new boolean[rawArr.length];
-//        count = 0;
-//        permutation(0, sb);
-//    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         String input;
         do {
             input = br.readLine();
-            if (input.length() <= 0) break;
             s = input.split(" ");
             rawArr = s[0].toCharArray();
-            res = Integer.parseInt(s[1]);
-            N = rawArr.length;
             selArr = new int[rawArr.length];
             isIn = new boolean[rawArr.length];
-            count = 0;
-            StringBuilder tmp = new StringBuilder();
-            permutation(0, tmp);
-            sb.append(tmp.length() > 0 ? tmp : input + " = " + "No permutation\n");
-            //System.out.println(tmp.length());
-            //input = (br.readLine());
-            //System.out.println(input);
+            count = 1;
+            StringBuilder sb = new StringBuilder();
+            permutation(0, sb);
+            result.append(input).append(" = ").append(sb.length() > 0 ? sb : "No permutation").append("\n");
         } while (br.ready());
         br.close();
-        System.out.println(sb);
+        System.out.println(result);
     }
 }
