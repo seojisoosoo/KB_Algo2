c1691b84e3b2765ce9c51348a0091720bc3f2ceb
rninji <yellowminji@naver.com>
2023-03-15 13:31:17 +0900

    B1068_트리_길민지

---
+++ src/Day5/B1068_트리/B1068_트리_길민지.java
@@ -0,0 +1,59 @@
+package Day5.B1068_트리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1068_트리_길민지 {
+	static int N, delN; // 노드 개수, 지울 노드
+	static int rootN; // 루트노드
+	static int parent[]; // 부모 배열
+	static boolean isVisited[]; // 방문 여부
+	static int cnt; // 리프노드 개수
+	
+	static void findLeaf(int node) {
+		isVisited[node] = true; // 방문 체크
+		
+		boolean isLeaf = true;
+		for (int i=0; i<N; i++) {
+			if (isVisited[i] == false && parent[i] == node) { // 자식 노드가 있다면
+				isLeaf = false; // 리프 노드 X
+				findLeaf(i); // 자식 노드 방문
+			}
+		}
+		if (isLeaf == true) { // 리프노드라면
+			cnt++; 
+			return; // 재귀 종료
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		// 첫번째줄 입력
+		N = Integer.parseInt(st.nextToken());
+		
+		// 초기화
+		parent = new int[N];
+		isVisited = new boolean[N];
+		rootN = -1;
+		
+		// 두번째줄 입력
+		st = new StringTokenizer(bf.readLine());
+		for(int i=0; i<N; i++) {
+			parent[i] = Integer.parseInt(st.nextToken());
+			if (parent[i]==-1) rootN = i;
+		}
+		
+		// 세번째줄 입력
+		st = new StringTokenizer(bf.readLine());
+		delN = Integer.parseInt(st.nextToken());
+		isVisited[delN] = true; // 지운 노드는 방문처리해서 그 아래로 탐색하지 않게 함
+		
+		if (delN!=rootN) findLeaf(rootN); // 지운 노드가 루트 노드라면 cnt = 0
+		System.out.println(cnt);
+	}
+
+}
