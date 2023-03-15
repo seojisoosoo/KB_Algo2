f138d0144934622ebfa6c0e784a25023f62abffd
jaenGit <dksehgis@androidjava.com>
2023-03-15 12:38:29 +0900

    B11725_트리의부모찾기_Sol

--- src/Day5/B11725_트리의부모찾기/B11725_트리의부모찾기_Sol.java
+++ src/Day5/B11725_트리의부모찾기/B11725_트리의부모찾기_Sol.java
@@ -1,22 +1,32 @@
-package Day5;
+package Day5.B11725_트리의부모찾기;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Tree_ArrayList {
+public class B11725_트리의부모찾기_Sol {
 
 	static int N;
 	
 	static ArrayList<Integer> tree [];
 	
 	static boolean [] isVisited;
-
+	static int [] parent;
+	
+	static void Search(int node) {
+		
+		System.out.println("Node : " + node);
+		for(int nextNode : tree[node]) {
+			if(isVisited[nextNode]) continue;
+			isVisited[nextNode] = true;
+			Search(nextNode);
+			parent[nextNode] = node;
+		}
+	}
 	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
 		N = sc.nextInt();
 		tree = new ArrayList[N + 1];
 		isVisited = new boolean [N+1];
@@ -36,8 +46,13 @@ public class Tree_ArrayList {
 			System.out.println(i + " 노드 : " + tree[i]);
 		}
 		
+		parent = new int [N+1];
 
-		// RootNode 부터 탐색
+		// RootNode 부터 탐색 //
+		isVisited[1] = true;
+		Search(1);
+	
+		for(int i=2; i<N+1; i++) System.out.println(parent[i]);
 
 	}
 }
