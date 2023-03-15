4cd385a5fd5342a1a953be5bda693bc309130b6e
gyeoul <clsmay.1st@gmail.com>
2023-03-15 09:27:05 +0900

    B1182_부분수열의합_박창현_코드정리

--- src/Day5/B1182_부분수열의합/B1182_부분수열의합_박창현.java
+++ src/Day5/B1182_부분수열의합/B1182_부분수열의합_박창현.java
@@ -6,45 +6,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B1182_부분수열의합_박창현 {
-    static int n;
     static int[] arr;
     static boolean[] isSel;
     static int result = 0;
-    static int S=0;
+    static int N = 0;
+    static int S = 0;
+
     static void subset(int num) {
-        if (num == n) {
+        if (num == N) {
             int sum = 0;
             boolean flag = false;
-            for (int i = 0; i < n; i++) {
+
+            for (int i = 0; i < N; i++) {
                 if (isSel[i]) {
-                    flag = true;
-                    //System.out.print(arr[i] + " ");
+                    flag = true; //공집합 걸러내기
                     sum += arr[i];
                 }
             }
-//            System.out.println(sum);
-            if (S==sum&&flag)result++;
+
+            if (S == sum && flag) result++;
             return;
         }
+
         isSel[num] = false;
-        subset(num+1);
+        subset(num + 1);
+
         isSel[num] = true;
-        subset(num+1);
+        subset(num + 1);
     }
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int N = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
+
         arr = new int[N];
+        isSel = new boolean[N];
+
         st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        n = arr.length;
-        isSel = new boolean[n];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
         subset(0);
         System.out.println(result);
     }
