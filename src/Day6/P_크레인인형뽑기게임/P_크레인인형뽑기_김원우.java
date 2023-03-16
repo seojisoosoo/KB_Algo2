4f2954688039f755e7d2b0095c94b88592ae2c21
kim <dnjsdn2468@naver.com>
2023-03-16 17:10:41 +0900

    P_크레인인형뽑기_김원우

--- src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기_김원우.java
+++ src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기_김원우.java
@@ -7,14 +7,12 @@ public class P_크레인인형뽑기_김원우 {
 
         int len_move = moves.length;
         int len_board = board.length;
-
-//        int[] bucket = new int[len_move];
         Stack<Integer> bucket = new Stack<>();
+
         int cnt = 0;
         int ans = 0;
 
         for (int i = 0; i < len_move; i++) {
-
             for (int j = 0; j < len_board; j++) {
                 if (board[j][moves[i]-1] != 0) {
 
@@ -30,18 +28,14 @@ public class P_크레인인형뽑기_김원우 {
                 }
             }
         }
-//        System.out.println(bucket);
         System.out.println(ans);
 
         int answer = ans;
         return answer;
     }
-
     public static void main(String[] args) {
-
         int[][] board = new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
         int[] moves = new int[]{1, 5, 3, 5, 1, 2, 1, 4};
-
         solution(board, moves);
 
     }
