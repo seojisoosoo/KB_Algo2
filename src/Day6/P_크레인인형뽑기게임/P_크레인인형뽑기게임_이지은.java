9b8f460153eb2d3f2d620ee3044033057a6d79b4
lje71421@naver.com <lje71421@naver.com>
2023-03-16 17:09:31 +0900

    P_크레인인형뽑기게임_수정

--- src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기게임_이지은.java
+++ src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기게임_이지은.java
@@ -13,26 +13,17 @@ class Solution {
             now = moves[i]-1;
             for(int j=0; j<board.length; j++){
                 if(board[j][now] != 0){ //0이 아니면 
-                    if(stack.isEmpty()){
-                        stack.push(board[j][now]); //stack에 삽입
-                        board[j][now] = 0; //빼낸 인형은 0으로 변경
-                        break;
+                    if(!stack.isEmpty() && stack.peek() == board[j][now]){
+                        stack.pop();
+                        answer += 2;
                     }else{
-                        if(stack.peek() == board[j][now]){
-                            board[j][now] = 0; // 0으로 변경
-                            stack.pop();
-                            answer += 2;
-                            break;
-                        } else{
-                            stack.push(board[j][now]); //stack에 삽입
-                            board[j][now] = 0; //빼낸 인형은 0으로 변경
-                            break;
-                        } 
+                        stack.push(board[j][now]); //stack에 삽입
                     }  //else문 종료
-                } // != 0 조건 if 문
+                    board[j][now] = 0; //빼낸 인형은 0으로 변경
+                    break;
+                } 
             } // for 문
         }
-        
         return answer;
     }
 }
