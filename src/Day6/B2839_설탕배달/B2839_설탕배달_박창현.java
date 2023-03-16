9c2160a7f059129a073c0c530f5ac2d9699c1a71
gyeoul <clsmay.1st@gmail.com>
2023-03-16 12:59:27 +0900

    B2839_설탕배달_박창현_코드정리

--- src/Day6/B2839_설탕배달/B2839_설탕배달_박창현.java
+++ src/Day6/B2839_설탕배달/B2839_설탕배달_박창현.java
@@ -9,17 +9,18 @@ public class B2839_설탕배달_박창현 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
+        int a = 3, b = 5;
 
         int[] arr = new int[N + 1];
         Arrays.fill(arr, 0);
-        if (N >= 3) arr[3] = 1;
-        if (N >= 5) arr[5] = 1;
+        if (N >= a) arr[a] = 1;
+        if (N >= b) arr[b] = 1;
 
-        for (int i = 5; i < N + 1; i++) {
-            if (arr[i - 3] == 0 && arr[i - 5] == 0) continue;
-            if (arr[i - 3] == 0) arr[i] = arr[i - 5] + 1;
-            else if (arr[i - 5] == 0) arr[i] = arr[i - 3] + 1;
-            else arr[i] = Math.min(arr[i - 3], arr[i - 5]) + 1;
+        for (int i = Math.max(a, b); i < N + 1; i++) {
+            if (arr[i - a] == 0 && arr[i - b] == 0) continue;
+            if (arr[i - a] == 0) arr[i] = arr[i - b] + 1;
+            else if (arr[i - b] == 0) arr[i] = arr[i - a] + 1;
+            else arr[i] = Math.min(arr[i - a], arr[i - b]) + 1;
         }
 
         if (arr[N] == 0) System.out.println(-1);
