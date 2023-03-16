a3254f1f661923d3a80b9914997c987f9e9c5dce
Gyu <tmdrb0912@naver.com>
2023-03-16 17:46:55 +0900

    P_크레인인형뽑기게임_곽승규

--- src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기게임_곽승규.java
+++ src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기게임_곽승규.java
@@ -2,7 +2,7 @@ package Day6.P_크레인인형뽑기게임;
 
 import java.util.Stack;
 
-public class P_크레인인형뽑기_곽승규 {
+public class P_크레인인형뽑기게임_곽승규 {
 
 	public static void main(String[] args) {
 		int [][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
