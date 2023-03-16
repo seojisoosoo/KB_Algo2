21debe45f8fa97304e08c0046b6641c214ce0567
gyeoul <clsmay.1st@gmail.com>
2023-03-16 11:30:17 +0900

    B2839_설탕배달_박창현

---
+++ src/Day6/B2839_설탕배달/B2839_설탕배달_박창현.java
@@ -0,0 +1,29 @@
+package Day6.B2839_설탕배달;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2839_설탕배달_박창현 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int count = 0;
+        int[] arr = new int[N+1];
+        Arrays.fill(arr,0);
+        if(N>=3) arr[3] = 1;
+        if(N>=5) arr[5] = 1;
+        for (int i = 5; i < N+1; i++) {
+            if (arr[i-3]==0&&arr[i-5]==0)continue;
+            if (arr[i-3]==0)
+                arr[i] = arr[i-5]+1;
+            else if (arr[i-5]==0)
+                arr[i] = arr[i-3]+1;
+            else arr[i]=Math.min(arr[i-3],arr[i-5])+1;
+        }
+        if (arr[N]==0) System.out.println(-1);
+        else System.out.println(arr[N]);
+        //System.out.println(Arrays.toString(arr));
+    }
+}
