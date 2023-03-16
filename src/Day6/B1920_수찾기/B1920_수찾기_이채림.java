4378f4b347044c1a9ddc59f590da67256fbf2d04
lee5917 <gdiop74@naver.com>
2023-03-16 16:14:37 +0900

    B1920_수찾기_이채림

---
+++ src/Day6/B1920_수찾기/B1920_수찾기_이채림.java
@@ -0,0 +1,53 @@
+package Day6.B1920_수찾기;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B1920_수찾기_이채림 {
+	static int N;
+	static int M;
+	static int[] arr;
+	static int low;
+	static int high;
+
+	static void BinarySearch(int target) {
+		low = 0;
+		high = N - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (arr[mid] == target) {
+				System.out.println(1);
+				return;
+			}
+			else if (arr[mid] > target)
+				high = mid - 1;
+			else
+				low = mid + 1;
+		}
+		System.out.println(0);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();// 배열 개수
+		arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		M = sc.nextInt(); // 배열 안에서 찾을 개수
+		
+		int result [] = new int [M];
+		
+		for (int i = 0; i < M; i++) {
+			int val = sc.nextInt();
+			BinarySearch(val);
+		
+
+		}
+	}
+
+}
