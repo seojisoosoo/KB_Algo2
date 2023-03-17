1bc74e64de03277a5ab46127987cbe861a08004a
jaenGit <dksehgis@androidjava.com>
2023-03-17 09:03:57 +0900

    add Day7

--- src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기_Sol.java
+++ src/Day6/P_크레인인형뽑기게임/P_크레인인형뽑기_Sol.java
@@ -1,5 +1,4 @@
 package Day6.P_크레인인형뽑기게임;
-
 import java.util.*;
 import java.io.*;
 import java.io.IOException;
@@ -9,22 +8,26 @@ import java.util.Stack;
 
 public class P_크레인인형뽑기_Sol {
 	public int solution(int[][] board, int[] moves) {
-		int answer = 0;
-
-		Stack <Integer> stack = new Stack<>();
-		stack.push(0);  // stack 이 비어있는 경우를 방지 
+		
+		int answer = 0;						
+		Stack <Integer> stack = new Stack<>(); 	// Stack 이용
+		stack.push(0);  						// Stack 이 비어있는 경우를 방지 
 
 		// move -> x 열 선택
 		for (int move : moves) {
+			// y -> 위에서부터 확인
 			for (int y = 0; y < board.length; y++) {
+				// 인형이 나오면 집음
 				if (board[y][move - 1] != 0) {
+					// 인형 터지면 pop
 					if (stack.peek() == board[y][move - 1]) {
 						stack.pop();
 						answer += 2;
 					} 
-					else {
+					// 인형 터지지 않으면 push
+					else 
 						stack.push(board[y][move - 1]);
-					}
+					// 인형 집으니깐 없어짐
 					board[y][move - 1] = 0;
 					break;
 				}
---
+++ src/Day7.java
@@ -0,0 +1,10 @@
+
+public class Day7 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
