fb21c3a689e5da8c2d2014fb07a2e9f727f609a7
gyeoul <clsmay.1st@gmail.com>
2023-03-17 11:01:39 +0900

    B1991_트리순회_박창현_주석추가

--- src/Day7/B1991_트리순회/B1991_트리순회_박창현.java
+++ src/Day7/B1991_트리순회/B1991_트리순회_박창현.java
@@ -12,6 +12,8 @@ public class B1991_트리순회_박창현 {
     static StringBuilder sb;
 
     static void dfs(int nodeidx, int logic) {
+        // '.'-'A' 값은 -19
+        // 로직: 0-전위순회 1-중위순회 2-후위순회
         if (logic == 0) sb.append((char) (arr[nodeidx][0] + 'A'));
         if (arr[nodeidx][1] >= 0) dfs(arr[nodeidx][1], logic);
         if (logic == 1) sb.append((char) (arr[nodeidx][0] + 'A'));
@@ -30,12 +32,19 @@ public class B1991_트리순회_박창현 {
             arr[i][1] = st.nextToken().charAt(0) - 'A';
             arr[i][2] = st.nextToken().charAt(0) - 'A';
         }
+        // 값이 항상 정렬되어 들어오는것이 아니므로
+        // arr[n][0]값과 인덱스를 동일하게 만들기 위해 arr 정렬
+        // "노드의 이름은 A부터 차례대로 알파벳 대문자로 매겨지며" 라는 조건이 있기 때문에
+        // 정렬하면 항상 인덱스와 해당 노드의 값이 일치함.
         Arrays.sort(arr, (Comparator.comparingInt(o -> o[0])));
         sb = new StringBuilder();
+        // 전위순회
         dfs(0, 0);
         sb.append("\n");
+        // 중위순회
         dfs(0, 1);
         sb.append("\n");
+        // 후위순회
         dfs(0, 2);
         System.out.println(sb);
     }
