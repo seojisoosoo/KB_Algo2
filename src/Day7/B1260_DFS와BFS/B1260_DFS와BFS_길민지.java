db9aa6b5fb1cf14a1761ecf8128946ad2a7b44bc
rninji <yellowminji@naver.com>
2023-03-17 13:57:36 +0900

    B1260_DFS와BFS_길민지

--- src/Day7/B1260_DFS와BFS/B1260_DFS와BFS_길민지.java
+++ src/Day7/B1260_DFS와BFS/B1260_DFS와BFS_길민지.java
@@ -7,6 +7,7 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class B1260_DFS와BFS_길민지 {
@@ -14,28 +15,55 @@ public class B1260_DFS와BFS_길민지 {
 	static ArrayList <Integer> graph[];
 	static boolean isVisited[];
 	
-	static Queue <Integer>que = new ArrayDeque();
-	
+	// 재귀 사용
 	static void DFS(int v) {
 		if (isVisited[v]==false) { // 방문하지 않았다면 출력하고 방문 처리
 			System.out.print(v + " ");
 			isVisited[v] = true;
 		}
-		for (int i=0; i<graph[v].size();i++) { // 자식 탐색
+		for (int i=0; i<graph[v].size();i++) { // 인접 노드 탐색
 			if (isVisited[graph[v].get(i)]==true) continue;
 			DFS(graph[v].get(i));
 		}
 	}
 	
+	
+	// 스택 사용
+	/*
+	static void DFS(int root) {
+		Stack <Integer> stack = new Stack();
+		
+		stack.add(root); // 루트 노드 push
+		
+		while(!stack.isEmpty()) {
+			int node = stack.pop();
+			
+			if (isVisited[node]==false) { // 방문하지 않았다면 출력하고 방문 처리
+				System.out.print(node+" ");
+				isVisited[node] = true;
+			}
+			
+			for (int next : graph[node]) { // 인접 노드 탐색
+				if (isVisited[next]==true) continue;
+				stack.add(next); // push
+			}
+		}
+	}
+	*/
+	
 	static void BFS(int root) {
-		que.add(root); // 루트 노드 삽입
+		Queue <Integer>que = new ArrayDeque<Integer>();
+		
+		que.add(root); // 루트 노드 Enqueue
 		isVisited[root] = true;
+		
 		while (!que.isEmpty()) {
 			int node = que.poll(); // Dequeue
 			System.out.print(node+" ");
-			for (int i=0; i<graph[node].size();i++) { // 자식 탐색
+			
+			for (int i=0; i<graph[node].size();i++) { // 인접 노드 탐색
 				if (isVisited[graph[node].get(i)]==true) continue;
-				que.add(graph[node].get(i)); // 자식 Enqueue
+				que.add(graph[node].get(i)); // 인접 노드 Enqueue
 				isVisited[graph[node].get(i)] = true;
 			}
 		}
@@ -59,15 +87,21 @@ public class B1260_DFS와BFS_길민지 {
 			graph[v1].add(v2);
 			graph[v2].add(v1);
 		}
+		
+		// 정렬
 		for (int i=0; i<N+1; i++) Collections.sort(graph[i]);
 		
-		isVisited = new boolean[N+1];
+		// 스택 사용의 경우 내림차순 정렬
+		// for (int i=0; i<N+1; i++) Collections.sort(graph[i], Collections.reverseOrder());
 		
+		isVisited = new boolean[N+1];
 		DFS(V);
 		System.out.println();
 		
-		// isVisited 초기화
-		for (int i=0; i<N+1; i++) isVisited[i] = false;
+		// 다시 오름차순 정렬
+		// for (int i=0; i<N+1; i++) Collections.sort(graph[i]);
+		
+		isVisited = new boolean[N+1];
 		BFS(V);
 	}
 
