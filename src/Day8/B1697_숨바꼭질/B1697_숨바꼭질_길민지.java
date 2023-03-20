70c10b5e443ca1e27863e9c741cdfb74871a72a1
rninji <yellowminji@naver.com>
2023-03-20 10:41:36 +0900

    B1697_숨바꼭질_길민지

--- src/Day8/B1697_숨바꼭질/B1697_숨바꼭질_길민지.java
+++ src/Day8/B1697_숨바꼭질/B1697_숨바꼭질_길민지.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class B1697_숨바꼭질_길민지 {
 	static int N, K;
 	static int time[];
-	static int move [] = new int []{1, -1};
+	static int move [] = new int []{1, -1, 2};
 	
 	static void find() {
 		Queue<Integer> que = new ArrayDeque<>();
@@ -22,23 +22,17 @@ public class B1697_숨바꼭질_길민지 {
 		while(!que.isEmpty()) {
 			int now = que.poll();
 			
-			// 걷기
-			for (int i=0; i<2; i++) {
-				int next = now + move[i];
+			for (int i=0; i<3; i++) {
+				int next;
+				if (i==2) next = now * move[i]; // 순간이동
+				else next = now + move[i]; // 걷기
+				
 				if (next<0 || next>100_000 || time[next]!=0) continue;
-				time[next] = time[now]+1;
+				time[next] = time[now]+1; 
 				que.add(next);
 				if (next == K) return;
 			}
-			
-			// 순간이동
-			int next = now * 2;
-			if (next<0 || next>100_000 || time[next]!=0) continue;
-			time[next] = time[now]+1;
-			que.add(next);
-			if (next == K) return;
 		}
-		
 	}
 
 	public static void main(String[] args) throws IOException {
