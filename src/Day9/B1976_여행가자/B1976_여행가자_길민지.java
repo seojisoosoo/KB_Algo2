e0bc83d2729b89871ee414e4f989ef6dddcc445b
rninji <yellowminji@naver.com>
2023-03-21 15:46:29 +0900

    B1976_여행가자_길민지

--- src/Day9/B1976_여행가자/B1976_여행가자_길민지.java
+++ src/Day9/B1976_여행가자/B1976_여행가자_길민지.java
@@ -3,9 +3,13 @@ package Day9.B1976_여행가자;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+
+// 1. 유니온 파인드 사용
 public class B1976_여행가자_길민지 {
 	static int N, M;
 	static int parent[];
@@ -62,3 +66,78 @@ public class B1976_여행가자_길민지 {
 	}
 
 }
+
+
+// 2. ArrayList 사용
+/* 
+public class B1976_여행가자_길민지 {
+	static int N, M;
+	static ArrayList <Integer> link[];
+	static boolean [] isVisited;
+	
+	static boolean BFS(int start, int end) {
+		Queue <Integer> que = new ArrayDeque();
+		
+		que.add(start);
+		
+		isVisited = new boolean[N+1];
+		isVisited[start] = true;
+		
+		while(!que.isEmpty()) {
+			int now = que.poll();
+			for (int next : link[now]) {
+				if (next == end) return true;
+				if (isVisited[next]) continue;
+				isVisited[next] = true;
+				que.add(next);
+			}
+		}
+		
+		return false;
+	}
+	
+	public static void main(String [] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(bf.readLine());
+		
+		link = new ArrayList[N+1];
+		for (int i=1; i<N+1; i++) link[i] = new ArrayList();
+		
+		// 연결 정보 입력
+		for(int i=1; i<N+1; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for (int j=1; j<N+1; j++) {
+				int sel = Integer.parseInt(st.nextToken());
+				if (sel == 1) {
+					link[i].add(j);
+					link[j].add(i);
+				}
+			}
+		}
+		
+		// 여행 계획 입력
+		st = new StringTokenizer(bf.readLine());
+		
+		boolean flag = true; // 가능 여부
+		int start = Integer.parseInt(st.nextToken()); // 시작점
+		for (int i=1; i<M; i++) {
+			int end = Integer.parseInt(st.nextToken()); // 도착점
+			
+			if(start == end) continue; // 시작점과 도착점이 같은 경우 무조건 가능
+			
+			if (!BFS(start, end)) { // 갈 수 없다면 종료
+				flag = false;
+				break;
+			}
+			start = end;
+		}
+		
+		// 출력
+		if (flag) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
+*/
