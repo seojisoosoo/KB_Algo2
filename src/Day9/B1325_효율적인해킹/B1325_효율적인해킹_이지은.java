6b4d99e70be5cf56b10441e94a31a279e1661d22
lje71421@naver.com <lje71421@naver.com>
2023-03-21 14:04:16 +0900

    B1325_효율적인해킹_이지은

--- src/Day9/B1325_효율적인해킹/B1325_효율적인해킹_이지은.java
+++ src/Day9/B1325_효율적인해킹/B1325_효율적인해킹_이지은.java
@@ -6,41 +6,32 @@ package Day9.B1325_효율적인해킹;
  *      이 회사의 컴퓨터는 신뢰하는 관계와, 신뢰하지 않는 관계로 이루어져 있는데, A가 B를 신뢰하는 경우에는 B를 해킹하면, A도 해킹할 수 있다는 소리다.
  *      이 회사의 컴퓨터의 신뢰하는 관계가 주어졌을 때, 한 번에 가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호를 출력하는 프로그램을 작성하시오.
  * 
- * BFS 사용. 메모리가 크고, 시간이 오래 걸림 
+ * DFS 사용. 시간 단축 
  * */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class B1325_효율적인해킹_이지은 {
 	static int N,M;
 	static ArrayList <Integer> computer[];
 	static boolean [] isCheck;
-	static int [] numArr;  // 컴퓨터 번호 배열 
 	static int max = 0;
+	static int [] ans; //갯수를 담을 배열 
 	
-	static void bfs(int idx) {
-		int cnt = 0;
-		Queue <Integer> que = new ArrayDeque<>(); //큐 선언 
-		que.add(idx);
-		isCheck[idx] = true; //방문처리 
+	static void dfs(int idx) {
+		isCheck[idx] = true;
 		
-		while(!que.isEmpty()) { //큐가 빌때까지 
-			int nowIdx = que.poll();
-			cnt += 1;
-			for(int now : computer[nowIdx]) {
-				if(!isCheck[now]) {
-					que.add(now);
-					isCheck[now] = true;
-				}
+		for(int now : computer[idx]) {
+			//방문하지 않고, 간선이 존재할 때 
+			if(!isCheck[now]) {
+				ans[now]++;
+				dfs(now);
 			}
-		} //while 종료 
-		numArr[idx] = cnt;
-		if(max<cnt) max = cnt;  //max 값 비교 
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -59,20 +50,23 @@ public class B1325_효율적인해킹_이지은 {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-//			computer[a].add(b);
-			computer[b].add(a);
+			computer[a].add(b);
 		}
 		
-		numArr = new int[N+1];
-		
+		ans = new int[N+1];  
 		for(int i=1; i<N+1; i++) {
 			isCheck = new boolean[N+1];
-			bfs(i);  //bfs 돌림 
+			dfs(i);
 		} 
 		
-		for(int i=0; i<N+1; i++) {
-			if(numArr[i] == max) //최대값에 맞는 값의 인덱스만 출력하도록 
+		for(int i=1; i<N+1; i++) {
+			if(max < ans[i]) max = ans[i];
+		}
+		
+		for(int i=1; i<N+1; i++) {
+			if(ans[i] == max) //최대값에 맞는 값의 인덱스만 출력하도록 
 				System.out.print(i+" ");
 		}
 	}
 }
+
